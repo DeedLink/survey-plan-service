3fc0a94c5f7c9f994254172ce5775b8dd4980cfc
DulshanSiriwardhana <dulshansiriwardhana70@gmail.com>
2025-09-22 20:50:41 +0530

    cors added

--- src/index.js
+++ src/index.js
@@ -2,14 +2,16 @@ import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
 import planRoutes from "./routes/planRoutes.js";
+import cors from "cors";
 
 dotenv.config();
 connectDB();
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/plans", planRoutes);
 
-const PORT = process.env.PORT || 5002;
+const PORT = process.env.PORT || 5003;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
