c556f847514d705ef30ed5917dcc3f5993bb1ac7
DulshanSiriwardhana <dulshansiriwardhana70@gmail.com>
2025-10-28 00:12:05 +0530

    update

--- src/index.js
+++ src/index.js
@@ -14,15 +14,9 @@ app.use(express.json());
 app.use("/api/plans", planRoutes);
 
 const PORT = Number(process.env.PORT) || 5003;
-const IS_VERCEL = process.env.ON_VERCEL || false;
 
-console.log(IS_VERCEL);
+//app.listen(PORT, "0.0.0.0", () => console.log(`Server running on port ${PORT}`));
 
-if(IS_VERCEL){
 app.listen(PORT, async () => {
   console.log(`Server running on port ${PORT}`);
 });
-}
-else{
-    app.listen(PORT, "0.0.0.0", () => console.log(`Server running on port ${PORT}`));
-}
